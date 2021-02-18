7340f50c42a7e3aeaa1d44ebf073314474ffb69a
mks-94 <muralikrishna9424.com>
2021-02-18 20:04:54 +0530

    Scss is not working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import CtaTop from "./components/ctaTop/CtaTop";
-import "./styles/reset.css";
-import "./styles/cta-top.css";
 
 const App = () => {
   return <CtaTop />;
