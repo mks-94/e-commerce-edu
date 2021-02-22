197c82facaf5a1b84108b81becf97543eabcf5d3
mks-94 <muralikrishna9424.com>
2021-02-22 21:22:20 +0530

    Implemented Footer and routing.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "../src/components/header/Header";
+import Footer from "./components/footer/Footer";
+import Auth from "./pages/Auth";
 import Landing from "./pages/Landing";
 import "./styles/main.css";
 
 const App = () => {
-  return <Landing />;
+  return (
+    <Fragment>
+      <Router>
+        <Header />
+        <div className="page-container">
+          <Switch>
+            <Route exact path="/auth">
+              <Auth />
+            </Route>
+            <Route exact path="/">
+              <Landing />
+            </Route>
+          </Switch>
+        </div>
+        <Footer />
+      </Router>
+    </Fragment>
+  );
 };
 
 export default App;
--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,12 +1,10 @@
 import React, { Fragment } from "react";
 import CourseSlider from "../components/courseSlider/CourseSlider";
 import CtaTop from "../components/ctaTop/CtaTop";
-import Header from "../components/header/Header";
 
 const Landing = () => {
   return (
     <Fragment>
-      <Header />
       <CtaTop />
       <CourseSlider />
     </Fragment>
