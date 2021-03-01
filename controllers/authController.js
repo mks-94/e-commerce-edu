62da50c679ea720d80f68aeb3fdb5cd2386a4938
mks-94 <muralikrishna9424.com>
2021-03-01 21:33:54 +0530

    Implemented error handling.

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,6 +6,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRES = process.env.JWT_EXPIRES;
 const JWT_EXPIRATION_NUM = process.env.JWT_EXPIRATION_NUM;
 const NODE_ENV = process.env.NODE_ENV;
+const { ErrorHandler } = require("../utils/ErrorHandler");
 
 //Helper function
 const encryptPw = async (password) => {
@@ -36,9 +37,19 @@ const sendToken = (user, statusCode, req, res) => {
 };
 
 //Signup controller
-exports.signup = async (req, res) => {
+exports.signup = async (req, res, next) => {
   try {
     const { email, password } = req.body;
+
+    if (!email || !password) {
+      return next(new ErrorHandler(400, "Please enter an email and password."));
+    }
+
+    if (password.length < 8) {
+      return next(
+        new ErrorHandler(401, "Password must be at least 8 characters")
+      );
+    }
 
     const pw = await encryptPw(password);
     const newUser = await User.create({
@@ -48,21 +59,27 @@ exports.signup = async (req, res) => {
     newUser.password = null;
     sendToken(newUser, 201, req, res);
   } catch (err) {
-    console.log(err);
+    return next(err);
   }
 };
 
 //Login contoller
-exports.login = async (req, res) => {
+exports.login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return next(new ErrorHandler(400, "Incorrect email or password"));
+    }
     const compared = await bcrypt.compare(password, user.password);
     user.password = null;
-    compared
-      ? sendToken(user, 200, req, res)
-      : res.status(400).json({ message: "Login failed." });
+
+    if (!compared) {
+      return next(new ErrorHandler(400, "Incorrect email or password"));
+    }
+
+    sendToken(user, 200, req, res);
   } catch (err) {
-    console.log(err);
+    return next(err);
   }
 };
