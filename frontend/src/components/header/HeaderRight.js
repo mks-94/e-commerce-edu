12cbb17a74ac9efa199dca2447992a785870bc96
mks-94 <muralikrishna9424.com>
2021-03-02 21:49:14 +0530

    Im not able to fix the issue

--- frontend/src/components/header/HeaderRight.js
+++ frontend/src/components/header/HeaderRight.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import HeaderLogin from "./HeaderLogin";
+import HeaderMenu from "./HeaderMenu";
 
 const HeaderRight = () => {
   return (
     <div className="header__right">
       <AiOutlineShoppingCart className="header__right--cart" />
-      <HeaderLogin />
+      {/* <HeaderLogin /> */}
+      <HeaderMenu />
     </div>
   );
 };
