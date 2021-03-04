87a6949c90a9ef61a81d16bd7cd3902c4287869f
mks-94 <muralikrishna9424.com>
2021-03-04 21:48:53 +0530

    Implemented check user after refresh implemented

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.route("/signup").post(authController.signup);
 router.route("/login").post(authController.login);
 router.route("/logout").get(authController.logout);
+router.route("/checkuser").get(authController.checkUserCred);
 
 module.exports = router;
