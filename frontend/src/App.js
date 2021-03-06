1f7f8e2d1797e719c913c9470ef70c491e0ccc74
mks-94 <muralikrishna9424.com>
2021-03-06 22:42:01 +0530

    stripe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,27 +11,36 @@ import store from "./redux/store";
 import "./styles/main.css";
 import Alert from "./components/alert/Alert";
 
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
 const App = () => {
+  const stripePromise = loadStripe(
+    "pk_test_51IS2P9KgFOvyDQfDFFIGFRaFbo32kY3BHCOiZEXjuhRuXMlLAKB0h7boWF0CKf5RShRoPFPJ8bi8tAMmiTD3Fr3b00C7iiQQO5"
+  );
+
   return (
     <Provider store={store}>
       <Router>
         <Header />
         <div className="page-container">
           <Alert />
-          <Switch>
-            <Route exact path="/auth">
-              <Auth />
-            </Route>
-            <Route exact path="/product">
-              <Product />
-            </Route>
-            <Route exact path="/cart">
-              <ShoppingCart />
-            </Route>
-            <Route exact path="/">
-              <Landing />
-            </Route>
-          </Switch>
+          <Elements stripe={stripePromise}>
+            <Switch>
+              <Route exact path="/auth">
+                <Auth />
+              </Route>
+              <Route exact path="/product">
+                <Product />
+              </Route>
+              <Route exact path="/cart">
+                <ShoppingCart />
+              </Route>
+              <Route exact path="/">
+                <Landing />
+              </Route>
+            </Switch>
+          </Elements>
         </div>
         <Footer />
       </Router>
